e91bc66bd2f2ed65e15caf610665daeba7d30e41
alfonsosalinas2 <31940640+alfonsosalinas2@users.noreply.github.com>
2017-11-15 20:51:24 -0600

    add student.py file

---
+++ student.py
@@ -0,0 +1,15 @@
+class Student:
+
+    def __init__(self, student_id, first_name, last_name):
+        self.student_id = student_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __init__(self, student_id, first_name, last_name, enrollment_date):
+        self.student_id = student_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.enrollment_date = enrollment_date
+
+
+
